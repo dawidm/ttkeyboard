69765268b012d997a8bca8a91f027840f3e101d1
Dawid <dawidmt@gmail.com>
2019-07-25 01:18:52 +0200

    getting screen size in pixels (for distance calculations)

--- app/src/main/java/com/example/taptimingkeyboard/TapTimingKeyboard.java
+++ app/src/main/java/com/example/taptimingkeyboard/TapTimingKeyboard.java
@@ -1,14 +1,22 @@
 package com.example.taptimingkeyboard;
 
 import android.content.Context;
+import android.graphics.Point;
+import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 
 public class TapTimingKeyboard {
 
+    public static final String TAG = TapTimingKeyboard.class.getName();
+
     private View tapTimingKeyboardView;
 
-    public TapTimingKeyboard(Context context, TTKeyboardMotionEventListener motionEventListener) {
-        this.tapTimingKeyboardView = TTKeyboardLayout.qwertyLayout().generateView(context,motionEventListener);
+    public TapTimingKeyboard(Context context, TTKeyboardLayout.Layout layout, TTKeyboardMotionEventListener motionEventListener) {
+        Point point = new Point();
+        ((WindowManager)context.getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay().getRealSize(point);
+        Log.i(TAG,"screen size: "+point.x+"x"+point.y);
+        this.tapTimingKeyboardView = TTKeyboardLayout.withLayout(layout).generateView(context,motionEventListener);
     }
 
     public View getView() {
