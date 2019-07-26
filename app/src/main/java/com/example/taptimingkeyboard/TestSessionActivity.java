22d04593e4811d7d12b70f17d941d61821f71471
Dawid <dawidmt@gmail.com>
2019-07-26 22:37:05 +0200

    activity for running test session and test session logic

---
+++ app/src/main/java/com/example/taptimingkeyboard/TestSessionActivity.java
@@ -0,0 +1,149 @@
+package com.example.taptimingkeyboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class TestSessionActivity extends AppCompatActivity {
+
+    private TapTimingKeyboard tapTimingKeyboard;
+    private ArrayList<Long> clicksIds = new ArrayList<>();
+    private String[] words;
+    private int wordsIterator;
+    private char[] currentWord;
+    private int charsIterator;
+    private char currentChar = 0;
+
+    private TextView testWordTextView;
+    private TextView sessionInfoTextView;
+    private Button sessionStartButton;
+    private Button sessionStopButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_session);
+        testWordTextView = findViewById(R.id.test_word_textview);
+        sessionInfoTextView = findViewById(R.id.session_info_textview);
+        sessionStartButton = findViewById(R.id.start_button);
+        sessionStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startButtonClick(view);
+            }
+        });
+        sessionStopButton = findViewById(R.id.stop_button);
+        sessionStopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopButtonClick(view);
+            }
+        });
+        tapTimingKeyboard = new TapTimingKeyboard(getApplicationContext(), TTKeyboardLayout.Layout.SIMPLEST_QWERTY_SYMMETRIC, new TTKeyboardClickListener() {
+            @Override
+            public void onKeyboardClick(TTKeyboardButton ttButton, long clickId) {
+                checkKeyboardClick(ttButton, clickId);
+            }
+        });
+        tapTimingKeyboard.testSessionMode();
+        ((ConstraintLayout)findViewById(R.id.keyboard_container)).addView(tapTimingKeyboard.getView());
+    }
+
+    private void startSession() {
+        sessionInfoTextView.setText("User id #, session #");
+        sessionStartButton.setClickable(false);
+        sessionStopButton.setClickable(true);
+        //TODO generate session id and save timestamp
+        words=getResources().getStringArray(R.array.test_words);
+        wordsIterator=0;
+        currentWord=words[0].toCharArray();
+        charsIterator=0;
+        currentChar=currentWord[0];
+        clicksIds.clear();
+        loadWord();
+    }
+
+    private void endSession(boolean aborted) {
+        sessionInfoTextView.setText("User id #");
+        sessionStartButton.setClickable(true);
+        sessionStopButton.setClickable(false);
+        testWordTextView.setText("");
+        if (!aborted) {
+            //TODO save session info
+        }
+    }
+
+    private boolean nextWord() {
+        if(++wordsIterator>=words.length)
+            return false;
+        currentWord=words[wordsIterator].toCharArray();
+        charsIterator=0;
+        currentChar=currentWord[0];
+        loadWord();
+        return true;
+    }
+
+    private boolean nextChar() {
+        if (++charsIterator>=currentWord.length)
+            return false;
+        currentChar=currentWord[charsIterator];
+        return true;
+    }
+
+    private void loadWord() {
+        testWordTextView.setText(words[wordsIterator]);
+    }
+
+    private void resetWord() {
+        charsIterator=0;
+        currentChar=currentWord[0];
+    }
+
+    private void acceptWaitingClicks() {
+        Iterator<Long> iterator = clicksIds.iterator();
+        while (iterator.hasNext()) {
+            //TODO session id
+            tapTimingKeyboard.acceptButtonClick(iterator.next(),1);
+            iterator.remove();
+        }
+    }
+    private void rejectWaitingClicks() {
+        Iterator<Long> iterator = clicksIds.iterator();
+        while (iterator.hasNext()) {
+            tapTimingKeyboard.rejectButtonClick(iterator.next());
+            iterator.remove();
+        }
+    }
+
+    private void checkKeyboardClick(TTKeyboardButton ttButton, long clickId) {
+        if(ttButton.getCode()==currentChar) {   //correct keyboard click
+            clicksIds.add(clickId);
+            if(!nextChar()) {   //end of word, word correctly typed
+                acceptWaitingClicks();
+                if(!nextWord()) { //end of session
+                    endSession(false);
+                }
+            }
+        } else {
+            resetWord();
+            rejectWaitingClicks();
+        }
+    }
+
+    private void startButtonClick(View view) {
+        startSession();
+    }
+
+    private void stopButtonClick(View view) {
+        endSession(true);
+    }
+
+
+}
