9f44c45b77df71a6c29a9d755a15c7490748feee
Dawid <dawidmt@gmail.com>
2019-08-07 16:04:11 +0200

    TimingDataManager: use scheduled task to save data to db in test session mode

--- app/src/main/java/com/example/taptimingkeyboard/TimingDataManager.java
+++ app/src/main/java/com/example/taptimingkeyboard/TimingDataManager.java
@@ -3,61 +3,154 @@ package com.example.taptimingkeyboard;
 import android.content.Context;
 import android.util.Log;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
-//TODO check clickids by scheduled executor
 public class TimingDataManager {
 
     public static final String TAG = TimingDataManager.class.getName();
+    public static final long CHECK_ACCEPTED_CLICKS_INTERVAL_MS=100;
 
     private Context context;
     private boolean testSessionMode = false;
-    private long sessionId;
 
     //key = clickId
-    private Map<Long,KeyTapCharacteristics> keyTapCharacteristicsQueue = new HashMap<>();
+    private Map<Long,KeyTapCharacteristics> keyTapCharacteristicsQueue = Collections.synchronizedMap(new HashMap<Long, KeyTapCharacteristics>());
     //key = clickId of the second key
-    private Map<Long,FlightTimeCharacteristics> flightTimeCharacteristicsSecondId = new HashMap<>();
+    private Map<Long,FlightTimeCharacteristics> flightTimeCharacteristicsBySecondId = Collections.synchronizedMap(new HashMap<Long, FlightTimeCharacteristics>());
     //key = clickId of the first key
-    private Map<Long,FlightTimeCharacteristics> flightTimeCharacteristicsFirstId = new HashMap<>();
+    private Map<Long,FlightTimeCharacteristics> flightTimeCharacteristicsByFirstId = Collections.synchronizedMap(new HashMap<Long, FlightTimeCharacteristics>());
+    //first of second clickId has been accepted TODO hashCode
+    private Set<FlightTimeCharacteristics> flightTimeCharacteristicsAcceptedOnce = Collections.synchronizedSet(new HashSet<FlightTimeCharacteristics>());
 
-    private Set<Long> acceptedClickIds = new HashSet<>();
-    private Set<Long> rejectedClickIds = new HashSet<>();
+    private Set<Long> acceptedClickIds = Collections.synchronizedSet(new HashSet<Long>());
+    private Set<Long> rejectedClickIds = Collections.synchronizedSet(new HashSet<Long>());
+
+    private ScheduledExecutorService scheduledExecutorService;
 
     public TimingDataManager(Context context) {
         this.context = context;
     }
 
-    public void startTestSession(long sessionId) {
+    public void startTestSession() {
         testSessionMode=true;
-        this.sessionId=sessionId;
+        scheduledExecutorService=Executors.newScheduledThreadPool(1);
+        scheduledExecutorService.scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    checkAcceptedClicks();
+                } catch (Exception e) {
+                    Log.w(TAG,e);
+                }
+            }
+        },CHECK_ACCEPTED_CLICKS_INTERVAL_MS,CHECK_ACCEPTED_CLICKS_INTERVAL_MS, TimeUnit.MILLISECONDS);
+    }
+
+    private void checkAcceptedClicks() {
+        boolean clicksChecked=false;
+        if(!acceptedClickIds.isEmpty()) {
+            clicksChecked=true;
+            Iterator<Long> acceptedClicksIterator = acceptedClickIds.iterator();
+            while (acceptedClicksIterator.hasNext()) {
+                Long key = acceptedClicksIterator.next();
+                if(keyTapCharacteristicsQueue.containsKey(key)) {
+                    Log.d(TAG,"inserting keyTapCharacteristics " + keyTapCharacteristicsQueue.get(key).getKeyCharacter());
+                    TapTimingDatabase.instance(context).keyTapCharacteristicsDao().insertAll(keyTapCharacteristicsQueue.get(key));
+                    keyTapCharacteristicsQueue.remove(key);
+                }
+                if(flightTimeCharacteristicsByFirstId.containsKey(key)) {
+                    if(flightTimeCharacteristicsAcceptedOnce.contains(flightTimeCharacteristicsByFirstId.get(key))) {
+                        flightTimeCharacteristicsAcceptedOnce.remove(key);
+                        Log.d(TAG,"inserting flightTimeCharacteristics " + flightTimeCharacteristicsByFirstId.get(key).getCharFrom() + "->" + flightTimeCharacteristicsByFirstId.get(key).getCharTo());
+                        TapTimingDatabase.instance(context).flightTimeCharacteristicsDao().insertAll(flightTimeCharacteristicsByFirstId.get(key));
+                    } else {
+                        flightTimeCharacteristicsAcceptedOnce.add(flightTimeCharacteristicsByFirstId.get(key));
+                    }
+                    flightTimeCharacteristicsByFirstId.remove(key);
+                }
+                if(flightTimeCharacteristicsBySecondId.containsKey(key)) {
+                    if(flightTimeCharacteristicsAcceptedOnce.contains(flightTimeCharacteristicsBySecondId.get(key))) {
+                        flightTimeCharacteristicsAcceptedOnce.remove(key);
+                        Log.d(TAG,"inserting flightTimeCharacteristics " + flightTimeCharacteristicsBySecondId.get(key).getCharFrom() + "->" + flightTimeCharacteristicsBySecondId.get(key).getCharTo());
+                        TapTimingDatabase.instance(context).flightTimeCharacteristicsDao().insertAll(flightTimeCharacteristicsBySecondId.get(key));
+                    } else {
+                        flightTimeCharacteristicsAcceptedOnce.add(flightTimeCharacteristicsBySecondId.get(key));
+                    }
+                    flightTimeCharacteristicsBySecondId.remove(key);
+                }
+                acceptedClicksIterator.remove();
+            }
+        }
+        if (!rejectedClickIds.isEmpty()) {
+            clicksChecked=true;
+            Iterator<Long> rejectedClicksIterator = rejectedClickIds.iterator();
+            while (rejectedClicksIterator.hasNext()) {
+                Long key = rejectedClicksIterator.next();
+                keyTapCharacteristicsQueue.remove(key);
+                flightTimeCharacteristicsByFirstId.remove(key);
+                flightTimeCharacteristicsBySecondId.remove(key);
+                flightTimeCharacteristicsAcceptedOnce.remove(key);
+                rejectedClicksIterator.remove();
+            }
+        }
+        if(clicksChecked)
+            Log.d(TAG,String.format("ended checking accepted/rejected clicks; " +
+                            "keyTapCharacteristicsQueue size: %d, " +
+                            "flightTimeCharacteristicsByFirstId size: %d, " +
+                            "flightTimeCharacteristicsBySecondId size: %d, " +
+                            "flightTimeCharacteristicsAcceptedOnce size: %d",
+                    keyTapCharacteristicsQueue.size(),
+                    flightTimeCharacteristicsByFirstId.size(),
+                    flightTimeCharacteristicsBySecondId.size(),
+                    flightTimeCharacteristicsAcceptedOnce.size()));
     }
 
     public void endTestSession() {
         testSessionMode=false;
+        if(scheduledExecutorService!=null && !scheduledExecutorService.isShutdown())
+            scheduledExecutorService.shutdown();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                checkAcceptedClicks();
+            }
+        }).start();
     }
 
-    public void addKeyTapCharacteristics(KeyTapCharacteristics keyTapCharacteristics, long clickId) {
+    public void addKeyTapCharacteristics(final KeyTapCharacteristics keyTapCharacteristics, final long clickId) {
         Log.d(TAG,"keyTapCharacteristics: " + keyTapCharacteristics.getKeyCharacter());
         if(!testSessionMode) {
-            TapTimingDatabase.instance(context).keyTapCharacteristicsDao().insertAll(keyTapCharacteristics);
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    TapTimingDatabase.instance(context).keyTapCharacteristicsDao().insertAll(keyTapCharacteristics);
+                }
+            }).start();
         } else {
             keyTapCharacteristicsQueue.put(clickId,keyTapCharacteristics);
         }
     }
 
-    public void addFlightTimeCharacteristics(FlightTimeCharacteristics flightTimeCharacteristics, long firstClickId, long secondClickId) {
+    public void addFlightTimeCharacteristics(final FlightTimeCharacteristics flightTimeCharacteristics, final long firstClickId, final long secondClickId) {
         Log.d(TAG,"flightTimeCharacteristics: " + flightTimeCharacteristics.getCharFrom() + "->" + flightTimeCharacteristics.getCharTo());
         if(!testSessionMode) {
-            TapTimingDatabase.instance(context).flightTimeCharacteristicsDao().insertAll(flightTimeCharacteristics);
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    TapTimingDatabase.instance(context).flightTimeCharacteristicsDao().insertAll(flightTimeCharacteristics);
+                }
+            }).start();
         } else {
-            flightTimeCharacteristicsFirstId.put(firstClickId,flightTimeCharacteristics);
-            flightTimeCharacteristicsSecondId.put(secondClickId,flightTimeCharacteristics);
+            flightTimeCharacteristicsByFirstId.put(firstClickId,flightTimeCharacteristics);
+            flightTimeCharacteristicsBySecondId.put(secondClickId,flightTimeCharacteristics);
         }
     }
 
