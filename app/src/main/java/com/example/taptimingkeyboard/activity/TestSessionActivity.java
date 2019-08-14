a82cfc77b9723404ee6eccf55e2b5d8009fcf720
Dawid <dawidmt@gmail.com>
2019-08-14 12:39:21 +0200

    add error signals on failed word during test session

--- app/src/main/java/com/example/taptimingkeyboard/activity/TestSessionActivity.java
+++ app/src/main/java/com/example/taptimingkeyboard/activity/TestSessionActivity.java
@@ -2,10 +2,13 @@ package com.example.taptimingkeyboard.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.content.res.ResourcesCompat;
 import androidx.preference.PreferenceManager;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.media.AudioManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
@@ -34,14 +37,22 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class TestSessionActivity extends AppCompatActivity {
 
     public static final String TAG = TestSessionActivity.class.getName();
 
+    public static final int TEST_WORD_BLINK_TIME_MILLIS = 1000;
+    private static final float ERROR_SOUND_VOLUME = 0.5f;
+
     private TapTimingKeyboard tapTimingKeyboard;
+    private AudioManager audioManager;
 
     private long sessionId;
+    private boolean sessionActive=false;
     private ArrayList<Long> clicksIds = new ArrayList<>();
     private String[] words;
     private int wordsIterator;
@@ -55,6 +66,8 @@ public class TestSessionActivity extends AppCompatActivity {
     private Button sessionStopButton;
     private Spinner listsSpinner;
     private LinearLayout listLinearLayout;
+
+    private ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -123,7 +136,8 @@ public class TestSessionActivity extends AppCompatActivity {
         tapTimingKeyboard = new TapTimingKeyboard(getApplicationContext(), TTKeyboardLayout.Layout.SIMPLEST_QWERTY_SYMMETRIC, new TTKeyboardClickListener() {
             @Override
             public void onKeyboardClick(TTKeyboardButton ttButton, long clickId) {
-                checkKeyboardClick(ttButton, clickId);
+                if(sessionActive)
+                    checkKeyboardClick(ttButton, clickId);
             }
         });
         updateSessionInfo(false);
@@ -157,6 +171,7 @@ public class TestSessionActivity extends AppCompatActivity {
     }
 
     private void startSession(WordLists.WordList wordList) {
+        sessionActive=true;
         tapTimingKeyboard.startTestSession(sessionId);
         updateSessionInfo(true);
         words=wordList.getWordsCsv().split(",");
@@ -169,6 +184,7 @@ public class TestSessionActivity extends AppCompatActivity {
     }
 
     private void endSession(boolean aborted) {
+        sessionActive=false;
         tapTimingKeyboard.endTestSession();
         updateSessionInfo(false);
         sessionStartButton.setClickable(true);
@@ -250,8 +266,10 @@ public class TestSessionActivity extends AppCompatActivity {
         } else {
             if(charsIterator>0) {
                 resetWord();
-                checkKeyboardClick(ttButton, clickId);
+                //checkKeyboardClick(ttButton, clickId);
             }
+            testWordBlink();
+            errorSound();
             rejectWaitingClicks();
             tapTimingKeyboard.abortCurrentFlightTime();
         }
@@ -307,6 +325,27 @@ public class TestSessionActivity extends AppCompatActivity {
         while(it.hasNext())
             lists.add(it.next());
         listsSpinner.setAdapter(new ArrayAdapter<>(getApplicationContext(),R.layout.support_simple_spinner_dropdown_item,lists));
+    }
+
+    private void testWordBlink() {
+        testWordTextView.setTextColor(ResourcesCompat.getColor(getResources(),R.color.colorTestWordError,null));
+        scheduledExecutorService.schedule(new Runnable() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        testWordTextView.setTextColor(ResourcesCompat.getColor(getResources(),R.color.colorTestWord,null));
+                    }
+                });
+            }
+        },TEST_WORD_BLINK_TIME_MILLIS, TimeUnit.MILLISECONDS);
+    }
+
+    private void errorSound() {
+        if (audioManager == null)
+            audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        audioManager.playSoundEffect(AudioManager.FX_KEYPRESS_INVALID,ERROR_SOUND_VOLUME);
     }
 
 
