cdd09599c9a8fa41e6c886d02b1e0b5dda55a626
Dawid <dawidmt@gmail.com>
2019-09-04 17:47:01 +0200

    TapTimingKeyboard.java: javadoc, minor refactoring

--- app/src/main/java/com/example/taptimingkeyboard/keyboard/TapTimingKeyboard.java
+++ app/src/main/java/com/example/taptimingkeyboard/keyboard/TapTimingKeyboard.java
@@ -33,21 +33,25 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
+/**
+ * Main TapTimingKeyboard class
+ * This class creates keyboard View based on {@link TTKeyboardLayout} , maintains touch events and sends click events to {@link TTKeyboardClickListener}.
+ * The most important functionality of TapTimingKeyboard is saving data about user interaction with keys such as times between subsequent clicks, times of button hold, pressure of clicks, precision of clicks ({@link FlightTimeCharacteristics}, {@link KeyTapCharacteristics}).
+ */
 public class TapTimingKeyboard implements TTKeyboardMotionEventListener {
 
-    public static final String TAG = TapTimingKeyboard.class.getName();
+    private static final String TAG = TapTimingKeyboard.class.getName();
+    private static final float BUTTON_TEXT_PROPORTION = 0.4f;
+    private static final float INCH_TO_MM = 25.4f;
 
-    public static final float BUTTON_TEXT_PROPORTION = 0.4f;
-    public static final float INCH_TO_MM = 25.4f;
+    private final Context context;
+    private final UiSounds uiSounds;
+    private final TimingDataManager timingDataManager;
 
-    private Context context;
-    private UiSounds uiSounds;
-    private TimingDataManager timingDataManager;
-
-    private View tapTimingKeyboardView;
-    private TTKeyboardLayout ttKeyboardLayout;
-    private TTKeyboardClickListener clickListener;
-    private Map<TTKeyboardButton, Button> buttonsMap = new HashMap<>();
+    private final View tapTimingKeyboardView;
+    private final TTKeyboardLayout ttKeyboardLayout;
+    private final TTKeyboardClickListener clickListener;
+    private final Map<TTKeyboardButton, Button> buttonsMap = new HashMap<>();
 
     private double pixelSizeMmX;
     private double pixelSizeMmY;
@@ -63,13 +67,24 @@ public class TapTimingKeyboard implements TTKeyboardMotionEventListener {
     private int heightLandscape;
 
     //which buttons are currently pressed (but not released) and associated MotionEvents
-    private Map<TTKeyboardButton,KeyDownParameters> ttButtonsDownParametersMap = new HashMap<>();
+    private final Map<TTKeyboardButton,KeyDownParameters> ttButtonsDownParametersMap = new HashMap<>();
     private TTKeyboardButton lastTTButtonDown = null;
     private TTButtonClick lastTTButtonClick = null;
     //flight times of which second key have not been released yet
     private WaitingFlightTimeCharacteristics waitingFlightTimeCharacteristics = null;
     private long clickId = 0;
 
+    /**
+     * {@link TapTimingKeyboard} constructor
+     *
+     * Initializes keyboard View and keyboard logic
+     *
+     * @param context activity or service (when used as {@link android.view.inputmethod.InputMethod}) context
+     * @param layout defines keyboard buttons layout
+     * @param clickListener gets notified about events recognized as keyboard button clicks
+     * @param remotePreferences keyboard uses preferences set in application's {@link SharedPreferences} but some can be overridden by passing this object
+     * @param userId TapTimingKeyboard writes user interaction data to the database userId is taken from application SharedPreferences but it can be overridden by this parameter
+     */
     public TapTimingKeyboard(Context context, TTKeyboardLayout.Layout layout, TTKeyboardClickListener clickListener, @Nullable RemotePreferences remotePreferences, @Nullable Long userId) {
         this.context=context;
         this.clickListener=clickListener;
@@ -82,9 +97,14 @@ public class TapTimingKeyboard implements TTKeyboardMotionEventListener {
         if(vibrations)
             uiSounds.initVibrator();
         ttKeyboardLayout=TTKeyboardLayout.withLayout(layout);
-        tapTimingKeyboardView = createView(context);
+        tapTimingKeyboardView=createView();
     }
 
+    /**
+     * Loads preferences from application default SharedPreferences
+     * @param remotePreferences values passed in this object override values from SharedPreferences
+     * @param userId override user id from SharedPreferences
+     */
     private void loadPreferences(@Nullable RemotePreferences remotePreferences, @Nullable Long userId) {
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
         this.userId=(userId!=null)?userId:sharedPreferences.getLong("user_id",1);
@@ -96,6 +116,9 @@ public class TapTimingKeyboard implements TTKeyboardMotionEventListener {
         heightLandscape=(remotePreferences!=null&&remotePreferences.getSizeLandscape()!=null)?remotePreferences.getSizeLandscape():sharedPreferences.getInt("height_landscape",0);
     }
 
+    /**
+     * Uses android api to get screen parameters: size and DPI and. Sets keyboardHeightPixels using screen parameters.
+     */
     private void loadDisplayRelatedParameters() {
         Display display = ((WindowManager)context.getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
         Point point = new Point();
@@ -114,7 +137,13 @@ public class TapTimingKeyboard implements TTKeyboardMotionEventListener {
             keyboardHeightPixels=heightPortrait*0.01f*screenHeightPixels;
     }
 
-    private View createView(Context context) {
+    /**
+     * Creates keyboard {@link View} which is vertical {@link LinearLayout} containing keyboard buttons ({@link Button}) in rows (row is a horizontal {@link LinearLayout})
+     * {@link MotionEvent} from every button calls {@link #onMotionEvent(TTKeyboardButton, MotionEvent)}
+     *
+     * @return returns created {@link View}
+     */
+    private View createView() {
         float rowHeightPixels = keyboardHeightPixels/ttKeyboardLayout.getRows().size();
         LinearLayout mainLayout = new LinearLayout(new ContextThemeWrapper(context, R.style.AppTheme));
         mainLayout.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
@@ -159,27 +188,57 @@ public class TapTimingKeyboard implements TTKeyboardMotionEventListener {
         return mainLayout;
     }
 
+    /**
+     * Start test session mode.
+     * In normal mode user interaction parameters for every click are saved in database.
+     * In test session mode, click should be accepted by calling {@link #acceptButtonClick(long)} or rejected by calling {@link #rejectButtonClick(long)}.
+     * Only parameters associated with accepted clicks are saved into database.
+     * @param sessionId in session mode interaction data is saved with sessionId which should be generated by application maintaining test session (e.g. {@link com.example.taptimingkeyboard.activity.TestSessionActivity})
+     */
     public void startTestSession(long sessionId) {
         this.sessionId=sessionId;
         timingDataManager.startTestSession();
     }
 
+    /**
+     * Ends test session mode {@link #startTestSession(long)}
+     * TapTimingKeyboard returns to the "normal" mode
+     */
     public void endTestSession() {
         timingDataManager.endTestSession();
     }
 
+    /**
+     * Accepts button click (when test application recognized specific click as correct according to test session specifications)
+     * This should be used in test session mode {@link #startTestSession(long)}
+     * @param clickId click id received in {@link TTKeyboardClickListener#onKeyboardClick(TTKeyboardButton, long)}
+     */
     public void acceptButtonClick(long clickId) {
         timingDataManager.acceptButtonClick(clickId);
     }
 
+    /**
+     * Rejects button click (when test application recognized specific click as incorrect according to test session specifications)
+     * This should be used in test session mode {@link #startTestSession(long)}
+     * @param clickId click id received in {@link TTKeyboardClickListener#onKeyboardClick(TTKeyboardButton, long)}
+     */
     public void rejectButtonClick(long clickId) {
         timingDataManager.rejectButtonClick(clickId);
     }
 
+    /**
+     * @return keyboard View which contains TapTimingKeyboard layout (buttons ordered according to {@link TTKeyboardLayout})
+     */
     public View getView() {
         return tapTimingKeyboardView;
     }
 
+    /**
+     * Get distance between two buttons in current keyboard layout
+     * @param from first button
+     * @param to seconds button
+     * @return distance in mm (calculated using data from Android's {@link DisplayMetrics}) between top-left edges of specified buttons
+     */
     private double getButtonDistanceMillimeters(TTKeyboardButton from, TTKeyboardButton to) {
         Point distancesPx = getButtonDistancePx(from,to);
         double distanceXMm=distancesPx.x*pixelSizeMmX;
@@ -187,6 +246,12 @@ public class TapTimingKeyboard implements TTKeyboardMotionEventListener {
         return Math.sqrt(Math.pow(distanceXMm,2)+Math.pow(distanceYMm,2));
     }
 
+    /**
+     * Get distance between two buttons in current keyboard layout
+     * @param from first button
+     * @param to seconds button
+     * @return distance in pixels between top-left edges of specified buttons
+     */
     private Point getButtonDistancePx(TTKeyboardButton from, TTKeyboardButton to) {
         if(buttonsMap.isEmpty())
             throw new IllegalStateException("called getButtonDistance before creating layout view");
@@ -205,14 +270,29 @@ public class TapTimingKeyboard implements TTKeyboardMotionEventListener {
         return new Point(Math.abs(xFrom-xTo),Math.abs(yFrom-yTo));
     }
 
+    /**
+     * @param ttButton
+     * @return returns button's size in X dimension
+     */
     private double getButtonSizeX(TTKeyboardButton ttButton) {
         return buttonsMap.get(ttButton).getWidth();
     }
 
+    /**
+     * @param ttButton
+     * @return returns button's size in Y dimension
+     */
     private double getButtonSizeY(TTKeyboardButton ttButton) {
         return buttonsMap.get(ttButton).getHeight();
     }
 
+    /**
+     * Handles motion events ({@link MotionEvent}) sent by keyboard's buttons.
+     * If button click is recognized, notification is sent to {@link TTKeyboardClickListener}, with unique clickId.
+     * User interaction parameters ({@link KeyTapCharacteristics}, {@link FlightTimeCharacteristics}) are sent to {@link TimingDataManager}
+     * @param ttButton button for which {@link MotionEvent} happened
+     * @param motionEvent motion event specification
+     */
     @Override
     public synchronized void onMotionEvent(TTKeyboardButton ttButton, MotionEvent motionEvent) {
         long currentTimestampMillis = System.currentTimeMillis();
@@ -291,21 +371,36 @@ public class TapTimingKeyboard implements TTKeyboardMotionEventListener {
         }
     }
 
+    /**
+     * Don't save {@link FlightTimeCharacteristics} between last and future click
+     * Use case: when hiding keyboard. Flight time for two clicks between which keyboard has been hidden and showed again probably isn't usable information.
+     */
     public void abortCurrentFlightTime() {
         lastTTButtonClick=null;
     }
 
+    /**
+     * Sends keyboard button click event with unique click id to {@link #clickListener}
+     * @param ttButton
+     * @return click id which has been sent
+     */
     private long sendClickEvent(TTKeyboardButton ttButton) {
         clickListener.onKeyboardClick(ttButton,clickId);
         return clickId++;
     }
 
+    /**
+     * Play sound on click if enabled
+     */
     private void playClick() {
         if(clickSound) {
             uiSounds.playClickSound(clickVol);
         }
     }
 
+    /**
+     * Vibrate on click if enabled
+     */
     private void vibrateClick() {
         if(vibrations) {
             uiSounds.vibrateMs(vibrationDuration);
