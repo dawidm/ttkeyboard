afec86bcb805e137c5c322a4a409708d4d85899b
Dawid <dawidmt@gmail.com>
2019-09-10 14:41:33 +0200

    Init database when app starts.

--- app/src/main/java/com/example/taptimingkeyboard/TapTimingKeyboardApplication.java
+++ app/src/main/java/com/example/taptimingkeyboard/TapTimingKeyboardApplication.java
@@ -4,10 +4,13 @@ import android.app.Application;
 
 import androidx.preference.PreferenceManager;
 
+import com.example.taptimingkeyboard.data.TapTimingDatabase;
+
 public class TapTimingKeyboardApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
         PreferenceManager.setDefaultValues(this, R.xml.preference_screen, false);
+        TapTimingDatabase.instance(getApplicationContext()).query("select 1",null); //init database
     }
 }
