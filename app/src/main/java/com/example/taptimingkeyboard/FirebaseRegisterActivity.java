f53bb5bee6dafe2266cf56f8f13a0d02594c2510
Dawid <dawidmt@gmail.com>
2020-02-10 00:50:46 +0100

    firebase login and register activities

---
+++ app/src/main/java/com/example/taptimingkeyboard/FirebaseRegisterActivity.java
@@ -0,0 +1,14 @@
+package com.example.taptimingkeyboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FirebaseRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_firebase_register);
+    }
+}
