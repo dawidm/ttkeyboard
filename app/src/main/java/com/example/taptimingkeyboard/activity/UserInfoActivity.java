419cf492e00eaf8dde0e7bacadaa6abfc942252f
Dawid <dawidmt@gmail.com>
2020-02-10 23:54:01 +0100

    add logout to UserInfoActivity options menu

--- app/src/main/java/com/example/taptimingkeyboard/activity/UserInfoActivity.java
+++ app/src/main/java/com/example/taptimingkeyboard/activity/UserInfoActivity.java
@@ -6,6 +6,8 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -17,6 +19,7 @@ import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.example.taptimingkeyboard.FireBaseLoginActivity;
 import com.example.taptimingkeyboard.R;
 import com.example.taptimingkeyboard.data.LayoutStringEnum;
 import com.example.taptimingkeyboard.data.TapTimingDatabase;
@@ -110,6 +113,25 @@ public class UserInfoActivity extends AppCompatActivity {
         });
         usersListViewContainer.bringToFront();
         checkDataForEmail();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add(getString(R.string.text_logout));
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case 0:
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(this, FireBaseLoginActivity.class);
+                startActivity(intent);
+                finish();
+                break;
+        }
+        return true;
     }
 
     @Override
