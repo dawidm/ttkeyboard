017c597f8d8eb238373dcfb4acaa46a37607a2ec
Dawid <dawidmt@gmail.com>
2020-02-10 01:39:25 +0100

    FirebaseRegisterActivity: implemented registration

--- app/src/main/java/com/example/taptimingkeyboard/FirebaseRegisterActivity.java
+++ app/src/main/java/com/example/taptimingkeyboard/FirebaseRegisterActivity.java
@@ -1,14 +1,63 @@
 package com.example.taptimingkeyboard;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class FirebaseRegisterActivity extends AppCompatActivity {
+
+    private static final String TAG = FirebaseRegisterActivity.class.getName();
+
+    private FirebaseAuth mAuth;
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private EditText editTextPasswordConfirm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_firebase_register);
+        editTextEmail = findViewById(R.id.edit_text_email);
+        editTextPassword = findViewById(R.id.edit_text_password);
+        editTextPasswordConfirm = findViewById(R.id.edit_text_password_confirm);
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    public void registerClick(View v) {
+        if (!editTextPassword.getText().toString().equals(editTextPasswordConfirm.getText().toString())) {
+            Toast.makeText(FirebaseRegisterActivity.this, getString(R.string.text_passwords_differ),
+                    Toast.LENGTH_SHORT).show();
+            return;
+        }
+        final Button button = (Button)v;
+        button.setEnabled(false);
+        mAuth.createUserWithEmailAndPassword(editTextEmail.getText().toString(), editTextPassword.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "account created");
+                            Toast.makeText(FirebaseRegisterActivity.this, getString(R.string.text_registration_succeed),
+                                    Toast.LENGTH_LONG).show();
+                            finish();
+                        } else {
+                            Log.w(TAG, "account registration failed", task.getException());
+                            Toast.makeText(FirebaseRegisterActivity.this, getString(R.string.text_registration_failed),
+                                    Toast.LENGTH_SHORT).show();
+                            button.setEnabled(true);
+                        }
+                    }
+                });
     }
 }
